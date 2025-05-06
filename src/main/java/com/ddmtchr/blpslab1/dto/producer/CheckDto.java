5924847da2089cc32be85971e3099b98a6e3717f
Mikhail Romanenko <rmr04489@gmail.com>
2025-05-06 21:39:16 +0300

    Lab 3 rabbitmq propeties, sender, dto

---
+++ src/main/java/com/ddmtchr/blpslab1/dto/producer/CheckDto.java
@@ -0,0 +1,28 @@
+package com.ddmtchr.blpslab1.dto.producer;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class CheckDto {
+
+    private String guest;
+    private String estate;
+    private Long amount;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDate startDate;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDate endDate;
+}
